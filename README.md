556e4fb61503859bca10585dc298ef169c63db97
Xiu <kevin.lantx@gmail.com>
2022-07-08 17:13:23 -0700

    compose_subject() and compose_body() implemnted

--- README.md
+++ README.md
@@ -4,14 +4,18 @@ A python script to check Bandwagon data usage and report result to your email. U
 ## How report looks like
     Subject: BWG Data Monitor: 25/1000 GB(2.5%) used
     Body:
-        2.5% data has been used while this cycle has passed 3.3%.
-        Request Time(CST) : 1970/01/02 
+        Report: 2.5% data has been used while this cycle has passed 3.3%.
+        Request Time(CST): 1970/01/02 
         Hostname:hostname
         Total Data: 1000 GB
         Used Data: 25 GB
         Percent Used: 2.5%
         Data Remaining:975 GB
-        Data Reset Time: 1970/01/01 (CST)
+        Current Cycle(CST): 01/02/1970 - 02/02/1970
+        Data Reset Time(CST): 1970/01/02
         Days counter:1/30 Days
-        Timezone info: Request Time(PST) = 1970/1/1, Data Reset Time(PST) = 1970/2/1
+        ---------------Timezone related info starts-----------------
+        Request Time(PST) = 1970/1/1
+        Current Cycle(PST): 01/01/1970 - 02/01/1970
+        Data Reset Time(PST) = 1970/2/1
         
--- bwg_data_report.py
+++ bwg_data_report.py
@@ -14,8 +14,10 @@ def main():
     # lookup_result = bul.bwg_lookup(config)
     lookup_result = {'hostname': 'Abandonments', 'plan_monthly_data': 1073741824000, 'monthly_data_multiplier': 1, 'data_counter': 946972627, 'data_next_reset': 1659829465}
     processed_result = data_process(lookup_result)
-    compose_subject(lookup_result)
-    compose_body(lookup_result)
+    subject = compose_subject(processed_result)
+    body = compose_body(processed_result)
+    print(subject)
+    print(body)
 
 # Process lookup result
 # INPUT-[lookup_result]:<Dict> Processed response from bwg api
@@ -37,9 +39,9 @@ def data_process(lookup_result):
     first = reset_pst.replace(day=1)  
     lastMonth = first - timedelta(days=1)
     start_pst = lastMonth.replace(day=reset_pst.day)
-    result["date_start_pst"] = start_pst.strftime("%B %d, %Y")
+    result["date_start_pst"] = start_pst.strftime("%m/%d/%Y")
     result["date_start_cst"] = start_pst.astimezone(cst_tz).strftime("%m/%d/%Y")
-    result["date_reset_pst"] =  reset_pst.strftime("%B %d, %Y")
+    result["date_reset_pst"] =  reset_pst.strftime("%m/%d/%Y")
     result["date_reset_cst"] =  reset_pst.astimezone(cst_tz).strftime("%m/%d/%Y")
     days_cycle = reset_pst - start_pst
     result["days_cycle"] = str(days_cycle.days)
@@ -54,21 +56,37 @@ def data_process(lookup_result):
     data_used_percent = (data_used_GB / data_total_GB) * 100
     result["data_used_GB"] = "{:.2f}".format(data_used_GB)
     result["data_total_GB"] = "{:0.0f}".format(data_total_GB)
+    result["data_remain_GB"] = "{:.2f}".format(data_total_GB - data_used_GB)
     result["data_used_percent"] = "{:.2f}".format(data_used_percent) + "%"
-    
+    return result 
 # Compose the subject of email
 # INPUT-[data]:<Dict> Processed result
 # OUTPUT: <String> the subject of email 
 def compose_subject(data):
-    subject_string = f"BWG Data Monitor: 25/1000 GB(2.5%) used" 
-
+    subject_string = f"BWG Data Monitor: {data['data_used_GB']}/{data['data_total_GB']} GB ({data['data_used_percent']}) used" 
+    return subject_string
 
 # Compose the body of email
 # INPUT-[data]:<Dict> Processed result
 # OUTPUT: <String> the body of email 
 def compose_body(data):
-    body_string = ""
-    
+    body_string = (f"Report: {data['data_used_percent']} data has been used while this cycle has passed {data['days_passed_percent']}.\n"
+                   f"Request Time(CST) :{data['time_now_cst']}\n"
+                   f"Hostname: {data['hostname']}\n"
+                   f"Total Data: {data['data_total_GB']} GB\n"
+                   f"Used Data: {data['data_used_GB']} GB\n"
+                   f"Percent Used: {data['data_used_percent']}\n"
+                   f"Data Remaining: {data['data_remain_GB']} GB\n"
+                   f"Current Cycle(CST): {data['date_start_cst']} - {data['date_reset_cst']}\n"
+                   f"Data Reset Time(CST): {data['time_reset_cst']}\n"
+                   f"Days Counter: {data['days_passed']}/{data['days_cycle']} days\n"
+                   "---------------Timezone related info starts-----------------\n"
+                   f"Request Time(PST) :{data['time_now_pst']}\n"
+                   f"Current Cycle(PST): {data['date_start_pst']} - {data['date_reset_pst']}\n"
+                   f"Data Reset Time(PST): {data['time_reset_pst']}"
+
+                  )
+    return body_string
 
 
 if __name__ == "__main__":
