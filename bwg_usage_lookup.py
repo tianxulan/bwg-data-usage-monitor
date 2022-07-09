99a7295c576d7949b956de2281df900ceb58f89e
Xiu <kevin.lantx@gmail.com>
2022-07-09 14:56:51 -0700

    add error message if api request failed

--- bwg_usage_lookup.py
+++ bwg_usage_lookup.py
@@ -3,7 +3,7 @@ import requests
 import json
 import yaml
 from urllib.parse import urlunsplit, urlencode
-
+import sys
 # Perform a request to bwg api, filter result and return data usage information
 # Input [config]:<Dict> User configuration read from config.yaml 
 # Output: <Dict> Information selected by 'lookup_params' in config.yaml
@@ -12,8 +12,12 @@ def bwg_lookup(config):
     url = build_url(config)
     response = requests.get(url)
     data = json.loads(response.content)  
-    result = {key:value for key,value in data.items() if key in config['bwg']['lookup_params']}
-    return result
+    if (data["error"] != 0):
+        print("Request to BWG api failed: " + data["message"])
+        sys.exit()
+    else:
+        result = {key:value for key,value in data.items() if key in config['bwg']['lookup_params']}
+        return result
 
 # Compose url form user configuration data
 # INPUT [config]: <Dict> User configuration read from config.yaml
